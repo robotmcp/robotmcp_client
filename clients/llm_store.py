f7f77a7ba70f29d8dc34a0fe84672a87bdc74b3c
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-11-03 22:06:38 +0530

    switched to groq models from cerebras

--- clients/llm_store.py
+++ clients/llm_store.py
@@ -1,16 +1,16 @@
-from langchain_cerebras import ChatCerebras
+from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def _get_cerebras_llm(model_name):
-    return ChatCerebras(
+def _get_groq_llm(model_name):
+    return ChatGroq(
         model=model_name,
         temperature=0.2,
         max_tokens=10240,
-        api_key=os.getenv("CEREBRAS_API_KEY"),
+        api_key=os.getenv("GROQ_API_KEY"),
     )
 
 
@@ -19,10 +19,10 @@ def get_llm(llm_name):
         "gemini": "google_genai:gemini-2.5-flash-lite",
         "openai": "openai:gpt-4.1",
         "claude": "anthropic:claude-sonnet-4-5",
-        "llama-scout": _get_cerebras_llm("llama-4-scout-17b-16e-instruct"),
-        "llama-8b": _get_cerebras_llm("llama3.1-8b"),
-        "llama": _get_cerebras_llm("llama-3.3-70b"),
-        "gpt-oss": _get_cerebras_llm("gpt-oss-120b"),
-        "qwen": _get_cerebras_llm("qwen-3-32b"),
+        "llama-scout": _get_groq_llm("llama-4-scout-17b-16e-instruct"),
+        "llama-8b": _get_groq_llm("llama-3.1-8b-instant"),
+        "llama": _get_groq_llm("llama-3.3-70b-versatile"),
+        "gpt-oss": _get_groq_llm("openai/gpt-oss-120b"),
+        # "qwen": _get_groq_llm("qwen-3-32b"),
     }
     return llms[llm_name]
